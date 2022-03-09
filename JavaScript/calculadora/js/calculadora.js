238d9f29765729e9a873d05d87ca027c64e52c2b
JesusPlasencia <jesusplasencia2508@hotmail.com>
2022-03-09 13:57:09 -0500

    Adding Calc & Other Projects

---
+++ JavaScript/calculadora/js/calculadora.js
@@ -0,0 +1,8 @@
+function suma()
+{
+    var n1 = parseFloat(document.getElementById('numero1').value);
+    var n2 = parseFloat(document.getElementById('numero2').value);
+    var result = n1 + n2;
+
+    document.getElementById('resultado').value = result;
+}
---
+++ JavaScript/sueldo/sueldo.js
@@ -0,0 +1,9 @@
+function sueldo(dT, hL)
+{
+    var sueldo = dT*hL*0.13*25;
+    return sueldo;
+}
+
+document.write('El sueldo es:')
+document.write('<br>')
+document.write(sueldo(21, 8));
